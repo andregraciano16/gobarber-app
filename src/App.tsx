c6fe11fb9be195c839a510880071607588824d0f
André Graciano <andregraciano16@gmail.com>
2020-09-12 19:58:41 -0300

    adicionando editorconfig, prettier e eslint

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import { View, StatusBar } from 'react-native';
 
 const App: React.FC = () => (
-    <>
-        <StatusBar barStyle="light-content" backgroundColor="#312e38" />
-        <View  style={{ flex: 1, backgroundColor: '#312e38'}} />
-    </>
+  <>
+    <StatusBar barStyle="light-content" backgroundColor="#312e38" />
+    <View style={{ flex: 1, backgroundColor: '#312e38' }} />
+  </>
 );
 
 export default App;
