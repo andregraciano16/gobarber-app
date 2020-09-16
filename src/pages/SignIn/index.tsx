4d5ab8e323ba07ddaacc8b533054cb0e24a1b9e3
André Graciano <andregraciano16@gmail.com>
2020-09-15 23:33:50 -0300

    montando a tela de cadastro

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -6,6 +6,7 @@ import {
   KeyboardAvoidingView,
   Platform,
 } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 import Icon from 'react-native-vector-icons/Feather';
 
 import Button from '../../components/Button';
@@ -22,6 +23,7 @@ import {
 } from './styles';
 
 const SignIn: React.FC = () => {
+  const navigation = useNavigation();
   return (
     <>
       <KeyboardAvoidingView
@@ -63,7 +65,7 @@ const SignIn: React.FC = () => {
 
         <CreateAccountButton
           onPress={() => {
-            console.log('click');
+            navigation.navigate('SignUp');
           }}
         >
           <Icon name="log-in" size={20} color="#ff9000" />
